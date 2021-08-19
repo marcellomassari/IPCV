51223d6ac7aab3708cda03baf1876571526ac5f5
Lorenzo Valente <s246628@studenti.polito.it>
2021-08-19 17:24:15 +0200

    versione in italiano

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
-#print(sr.Microphone.list_microphone_names())
 
 #microfono Lorenzo
 mic = sr.Microphone(device_index=1)
 
-#microfono Marcello
+#microfono Marcello e Massimiliano
 #mic = sr.Microphone(device_index=0)
 
 with mic as source:
-    print("Please wait. Calibrating microphone...")
-    r.adjust_for_ambient_noise(source, duration=5)
-    print("Say something!")
+    print("Aspetta. Sto calibrando il microdono...")
+    r.adjust_for_ambient_noise(source, duration=2)
+    print("Ora parla!!")
     audio = r.listen(source)
 
-print("I thinks you said '" + r.recognize_google(audio, language="it-IT") + "'")
+print("Hai detto '" + r.recognize_google(audio, language="it-IT") + "'")
 
 
