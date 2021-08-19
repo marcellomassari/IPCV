b7ed93752a7d37ef7da7fbbe475bff2a0783dfbd
Lorenzo Valente <s246628@studenti.polito.it>
2021-08-19 17:03:05 +0200

    prova speech 1

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello World!")
-print("general Kenobi")
+import speech_recognition as sr
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+with mic as source:
+    print("Please wait. Calibrating microphone...")
+    r.adjust_for_ambient_noise(source, duration=5)
+    print("Say something!")
+    audio = r.listen(source)
+
+print("I thinks you said '" + r.recognize_google(audio) + "'")
+
+
