0df812c5a2f637a7dc39aac2b554a45b8cf17af3
Lorenzo Valente <s246628@studenti.polito.it>
2021-08-19 17:09:22 +0200

    prova speech 2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
-mic = sr.Microphone()
+#print(sr.Microphone.list_microphone_names())
+
+mic = sr.Microphone(device_index=1)
 
 with mic as source:
     print("Please wait. Calibrating microphone...")
